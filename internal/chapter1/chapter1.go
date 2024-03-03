ae169158624ef940692fb29a8766576bbf9d5953
komugi8 <8448eito@gmail.com>
2024-03-04 00:13:14 +0900

    chapter1

--- internal/chapter1/chapter1.go
+++ internal/chapter1/chapter1.go
@@ -1,17 +1,29 @@
 package chapter1
 
-import "net/http"
-
-// revive:disable
+import (
+	"net/http"
+)
 
 func GetEcho(w http.ResponseWriter, r *http.Request) {
-	//FIXME: Getメソッドのアクセスか確認
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	params := r.Form
 
-	//FIXME: パラメータをFormに変換する
-
-	//FIXME: パラメータを取得する
-
-	//FIXME: パラメータをレスポンスに書き出す
-
-	//FIXME: レスポンスコード設定
+	for key, values := range params {
+		for _, value := range values {
+			_, err := w.Write([]byte(key + ": " + value + "\n"))
+			if err != nil {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+	w.WriteHeader(http.StatusOK)
 }
