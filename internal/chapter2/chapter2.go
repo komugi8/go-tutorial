9aaaea9c774013e07a7bcaa4ba4be09bcd9de41d
komugi8 <8448eito@gmail.com>
2024-03-04 01:01:22 +0900

    chapter2/get

--- internal/chapter2/chapter2.go
+++ internal/chapter2/chapter2.go
@@ -2,7 +2,21 @@ package chapter2
 
 import "net/http"
 
-// revive:disable
-
 func Get(w http.ResponseWriter, r *http.Request) {
+	req, err := http.NewRequest("GET", "http://localhost:8081/users?age=25", nil)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	req.Header.Set("key", "dip")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	w.WriteHeader(http.StatusOK)
 }
